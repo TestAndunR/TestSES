710c48828eeae062ea7a540f7a8fb5b7cccf542b
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-04-04 16:40:35 +0530

    Test SES commit 1

---
+++ TestSES/lambda.js
@@ -0,0 +1,30 @@
+let AWS = require('aws-sdk');
+const ses = new AWS.SES();
+exports.handler = function (event, context, callback) {
+	let message = event['message'];
+	
+	ses.sendEmail({
+		Destination: {
+			ToAddresses: ['andunranma@adroitlogic.com'],
+			CcAddresses: [],
+			BccAddresses: []
+		},
+		Message: {
+			Body: {
+				Text: {
+					Data: message
+				}
+			},
+			Subject: {
+				Data: 'TestingSES'
+			}
+		},
+		Source: 'kannangarar1993@gmail.com',
+	}, function (err, data) {
+		if (err) console.log(err, err.stack); // an error occurred
+		else console.log(data);           // successful response
+	});
+
+
+	callback(null, 'Successfully executed');
+}
