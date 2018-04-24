283093c0ca5369aa67450922e58a3376ab2da02f
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-04-24 09:58:52 +0530

    Test SES

--- TestSES/lambda.js
+++ TestSES/lambda.js
@@ -1,12 +1,11 @@
 let AWS = require('aws-sdk');
 const ses = new AWS.SES();
 exports.handler = function (event, context, callback) {
-	let message = event['message'];
-
+	let message = event.message;
 	ses.sendEmail({
 		Destination: {
-			ToAddresses: ['randika@adroitlogic.com'],
-			CcAddresses: [],
+			ToAddresses: ['kannangarar1993@gmail.com', 'andunranmal@gmail.com'],
+			CcAddresses: ['andunranmal@gmail.com'],
 			BccAddresses: []
 		},
 		Message: {
@@ -16,15 +15,14 @@ exports.handler = function (event, context, callback) {
 				}
 			},
 			Subject: {
-				Data: 'TestingSES'
+				Data: 'Test for SES'
 			}
 		},
-		Source: 'kannangarar1993@gmail.com'
+		Source: 'andun@adroitlogic.com',
 	}, function (err, data) {
 		if (err) console.log(err, err.stack); // an error occurred
 		else console.log(data);           // successful response
 	});
 
-
-	callback(null, "Success");
+	callback(null, 'Successfully executed');
 }
